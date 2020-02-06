ea6e6c5b361164441d6bbc95b3a13709bedab625
Evan Lyall <evan.lyall@gmail.com>
2020-02-05 21:12:31 -0800

    added training table; script to delete row

--- mysql/db_tools.py
+++ mysql/db_tools.py
@@ -3,46 +3,61 @@ import pandas as pd
 from metaflow import Metaflow, Flow
 
 TABLE_NAME = "metaflow"
-COLUMNS = ["flow", "run", "start", "finish"]
-DTYPE = db.types.String(32)
+COLUMN_NAMES = ["flow", "run", "start", "finish"]
+COLUMN_DTYPES = db.types.String(32)
+
+# TABLE_NAME = "training"
+# COLUMN_NAMES = ["flow", "run", "start"]
+# COLUMN_DTYPES = db.types.String(32)
+
 
 engine = db.create_engine('mysql+pymysql://db_user:uNw5^Hze6K&v24Z!PeCak*^n@ec2-52-38-203-135.us-west-2.compute.amazonaws.com/metaflow')
 metadata = db.MetaData()
-connection = engine.connect()
 
-def create_table(table_name=TABLE_NAME, columns=COLUMNS, dtype=DTYPE, if_exists="replace"):
+def create_table(table=TABLE_NAME, columns=COLUMN_NAMES, dtype=COLUMN_DTYPES, if_exists="replace"):
     df = pd.DataFrame(columns=columns)
-    df.to_sql(table_name, con=engine, if_exists=if_exists, dtype=dtype, index=False)
+    df.to_sql(table, con=engine, if_exists=if_exists, dtype=dtype, index=False)
 
-def delete_table(table_name=TABLE_NAME):
-    tbl = db.Table(table_name, metadata)
+def delete_table(table=TABLE_NAME):
+    tbl = db.Table(table, metadata)
     tbl.drop(engine) #deletes the table
 
-def append_flow(data, columns=COLUMNS, table_name=TABLE_NAME):
-    if isinstance(data, list): data = pd.DataFrame(data, columns=columns)
-    data.to_sql(table_name, con=engine, if_exists="append", index=False)
+def append_flow(data, columns=COLUMN_NAMES, table=TABLE_NAME):
+    if type(data) is list: 
+        data = [data] if type(data[0]) is not list else data
+        data = pd.DataFrame(data, columns=columns)
+    data.to_sql(table, con=engine, if_exists="append", index=False)
 
-def pull_from_s3(names="all", table_name=TABLE_NAME):
+def add_s3_flows(names="all", table=TABLE_NAME):
     if isinstance(names,str) and names=="all": 
         names = []
         flows = Metaflow().flows
         for flow in flows:
             names.append(flow.pathspec)
-    df = pd.DataFrame(columns=COLUMNS)
+    df = pd.DataFrame(columns=COLUMN_NAMES)
     for name in names:
         runs = list(Flow(name))
         for run in runs:
             index = run.path_components[1]
             start = run.created_at
             finish = run.finished_at
-            df = df.append(dict(zip(COLUMNS, [name, index, start, finish])), ignore_index=True)
-    df = df.sort_values(by=COLUMNS[:2])
-    append_flow(df, table_name=table_name)
+            df = df.append(dict(zip(COLUMN_NAMES, [name, index, start, finish])), ignore_index=True)
+    df = df.sort_values(by=COLUMN_NAMES[:2])
+    append_flow(df, table=table)
 
-def reset_table(table_name=TABLE_NAME, columns=COLUMNS, dtype=DTYPE):
-    create_table(table_name=table_name, columns=columns, dtype=dtype)
-    pull_from_s3(names="all", table_name=table_name)
+def reset_models_table(columns=COLUMN_NAMES, dtype=COLUMN_DTYPES):
+    create_tables(TABLE_NAME, columns=columns, dtype=dtype)
+    add_s3_flows(names="all", table=TABLE_NAME)
 
-def read_table(table_name=TABLE_NAME):
-    df = pd.read_sql(table_name, con=engine)
-    return(df)
+def read_table(table=TABLE_NAME):
+    return(pd.read_sql(table, con=engine))
+
+def delete_row(row, table=TABLE_NAME):
+    # row can be dict or single row of a dataframe
+    df = read_table(table)
+    tbl = db.Table(table, metadata, autoload=True, autoload_with=engine)
+    cond = df.apply(lambda row: db.and_(tbl.c['flow'] == row['flow'], tbl.c['run'] == row['run']), axis=1)
+    cond = db.or_(*cond)
+    delete = tbl.delete().where(cond)
+    with engine.connect() as conn:
+        conn.execute(delete)
