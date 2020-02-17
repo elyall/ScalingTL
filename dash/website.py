e2212576e275324aa437630ed5cab2525787877b
Evan Lyall <evan.lyall@gmail.com>
2020-02-16 16:41:23 -0800

    user can upload data

--- dash/website.py
+++ dash/website.py
@@ -1,170 +1,170 @@
 # -*- coding: utf-8 -*-
 
-# append ScalingTL & UniRep to path
+# append ScalingTL to path
 import sys
 if sys.platform == "linux" or sys.platform == "linux2":
     MODULE_PATH = '/home/ubuntu/ScalingTL/'
 elif sys.platform == "darwin":
     MODULE_PATH = '/Users/elyall/Dropbox/Projects/Insight/ScalingTL/'
 sys.path.append(MODULE_PATH)
+# loading registries
+from db_tools import read_table, append_row
+from data_IO import download_obj_from_s3, upload_files_to_s3
 
 # website
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-
-# loading data
 from dash_table import DataTable
-import base64
-import datetime
-import io
 import pandas as pd
-import os
 
 # running metaflow
 import subprocess
 
-# loading model registries
-from db_tools import read_table
-from data_IO import download_obj_from_s3
+# loading data for display
+import base64
+import io
+import os
 
 PAGE_SIZE = 20
 LOCAL_PATH = "tmp/"
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+BUCKET = "metaflow-metaflows3bucket-g7dlyokq680q"
 
 if not os.path.exists(LOCAL_PATH):
     os.makedirs(LOCAL_PATH) # create directory for objects pulled from S3
 df_trained = read_table("metaflow")
 df_running = read_table("training")
+df_datasets = read_table("datasets")
 
+
+# Create website
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = "ScalingTL"
-
 
 app.layout = html.Div(children=[
     html.H1(children='ScalingTL'),
     html.H6(children='''
         A scalable, version controlled transfer learning pipeline for UniRep.
     '''),
+
+    html.Hr(),
+    html.H3(children='Select a Starting Model'),
+    DataTable(
+        id='table_models',
+        data=df_trained.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in df_trained.columns],
+        page_size=PAGE_SIZE,
+        page_current=0,
+        row_selectable='single'
+    ),
+    html.Button(
+        ['Update'],
+        id='btn_models'
+    ),
+
+    html.Hr(),
+    html.Div(children='''
+        [Optional] Upload your own data:
+        Load a .csv file where the first column is amino acid sequences and any additional column is a measured feature where the feature label is the column name.
+    '''),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=False
+    ),
+    
+    html.Hr(),
+    html.H3(children='Select a Dataset'),
+    DataTable(
+        id='table_data',
+        data=df_datasets.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in df_datasets.columns],
+        page_size=PAGE_SIZE,
+        page_current=0,
+        row_selectable='single'
+    ),
+    html.Div(id='output-data-select'),
+
     html.Div(id='hidden-div', children=None, style={'display':'none'}),
     dcc.Tabs([
         dcc.Tab(label="Train", children=[
-            html.H3(children='Select Data'),
-
-            dcc.Dropdown(
-                id='select-data',
-                options=[
-                    {'label': 'Select a preloaded dataset', 'value': 'none'},
-                    {'label': 'MHC1', 'value': 'mhc1'},
-                ],
-                value='none'
-            ),
             
-            # html.H6(children='OR'),
-            # html.Div(children='''
-            #     Train on your own data: load a .csv file where the first column is amino acid sequences and any additional column is a measured feature
-            # '''),
-            # dcc.Upload(
-            #     id='upload-data',
-            #     children=html.Div([
-            #         'Drag and Drop or ',
-            #         html.A('Select Files')
-            #     ]),
-            #     style={
-            #         'width': '100%',
-            #         'height': '60px',
-            #         'lineHeight': '60px',
-            #         'borderWidth': '1px',
-            #         'borderStyle': 'dashed',
-            #         'borderRadius': '5px',
-            #         'textAlign': 'center',
-            #         'margin': '10px'
-            #     },
-            #     multiple=False
-            # ),
-            html.Div(id='output-data-select'),
-            # html.Div(id='output-data-upload'),
-
             html.Hr(),
             html.Button(
                 ['Train'],
                 id='btn_train',
             ),
+
             html.Hr(),
-
             html.H3(children='Training models:'),
-
             DataTable(
                 id='table_training',
                 data=df_running.to_dict('records'),
                 columns=[{"name": i, "id": i} for i in df_running.columns],
                 page_size=PAGE_SIZE,
-                page_current=0
+                page_current=0,
+                row_selectable='single'
             ),
-
             html.Button(
                 ['Update'],
                 id='btn_training'
             )
         ]),
 
-        dcc.Tab(label="Inference", children=[
-            html.H3(children='Trained models:'),
-
-            DataTable(
-                id='table_models',
-                data=df_trained.to_dict('records'),
-                columns=[{"name": i, "id": i} for i in df_trained.columns],
-                page_size=PAGE_SIZE,
-                page_current=0
-            ),
-
-            html.Button(
-                ['Update'],
-                id='btn_models'
-            )
+        dcc.Tab(label="Predict", children=[
+            html.Div(children='''
+                UNDER DEVELOPMENT
+            '''),
         ])
     ])
 ])
 
+# CALLBACK: SELECT A MODEL?
 
-# @app.callback(Output('output-data-upload', 'children'),
-#               [Input('upload-data', 'contents')],
-#               [State('upload-data', 'filename'),
-#                State('upload-data', 'last_modified')])
-# def update_output(contents, filename, date):
-#     if contents is not None:
-#         content_type, content_string = contents.split(',')
-#         decoded = base64.b64decode(content_string)
-#         ext = filename.split('.')[-1]
-#         try:
-#             if ext=='csv':
-#                 data_file = io.StringIO(decoded.decode('utf-8'))
-#                 df = pd.read_csv(data_file)
-#             elif ext=='xls':
-#                 data_file = io.BytesIO(decoded)
-#                 df = pd.read_excel(data_file)
-#         except Exception as e:
-#             print(e)
-#             return html.Div([
-#                 'There was an error processing this file.'
-#             ])
-#         df.to_csv(TMP)
-#         return html.Div([
-#             html.H5(filename),
-#             html.H6(datetime.datetime.fromtimestamp(date)),
-
-#             DataTable(
-#                 data=df.to_dict('records'),
-#                 columns=[{'name': i, 'id': i} for i in df.columns],
-#                 page_size=PAGE_SIZE
-#             ),
-
-#         ])
+# Upload data callback
+@app.callback(Output('table_data', 'data'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(contents, filename, date):
+    if contents is not None:
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        ext = filename.split('.')[-1]
+        try:
+            if ext=='csv':
+                data_file = io.StringIO(decoded.decode('utf-8'))
+                df = pd.read_csv(data_file, nrows=0)
+            elif ext=='xls':
+                data_file = io.BytesIO(decoded)
+                df = pd.read_excel(data_file, nrows=0)
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+        upload_files_to_s3(data_file, s3_path='data/', bucket=BUCKET) # upload data
+        df = pd.DataFrame({'name': filename, 'features': df.columns.tolist(), 'size': date, 'modified', date})
+        append_row(df, table="datasets") # upload registry to database
+        df_datasets = read_table("datasets")
+        return(df_datasets.to_dict('records'))
 
 @app.callback([Output('output-data-select', 'children'),
             Output('hidden-div', 'children')],
--- db_tools.py
+++ db_tools.py
@@ -1,46 +1,50 @@
 import sqlalchemy as db
 import pandas as pd
+# pull S3 models
 from metaflow import Metaflow, Flow
+# pull S3 datasets
+import boto3
+import os
+from datetime import datetime
 
-TABLE_NAME = "metaflow"
-COLUMN_NAMES = ["flow", "run", "start", "finish"]
-COLUMN_DTYPES = db.types.String(32)
 
-# TABLE_NAME = "training"
-# COLUMN_NAMES = ["flow", "run", "start"]
-# COLUMN_DTYPES = db.types.String(32)
+BUCKET = "metaflow-metaflows3bucket-g7dlyokq680q"
+DATA_PATH = "data/"
 
+
+# Create connection path
 create_path = 'mysql+pymysql://db_user:uNw5^Hze6K&v24Z!PeCak*^n@'
 import sys
 if sys.platform == "linux" or sys.platform == "linux2":
     create_path = create_path + '10.20.0.5'
 elif sys.platform == "darwin":
     create_path = create_path + 'ec2-44-230-85-65.us-west-2.compute.amazonaws.com'
-
 create_path = create_path + '/metaflow'
+
+# Create connection objects
 engine = db.create_engine(create_path)
 metadata = db.MetaData()
 
-def create_table(table=TABLE_NAME, columns=COLUMN_NAMES, dtype=COLUMN_DTYPES, if_exists="replace"):
-    df = pd.DataFrame(columns=columns)
-    df.to_sql(table, con=engine, if_exists=if_exists, dtype=dtype, index=False)
 
-def delete_table(table=TABLE_NAME):
+def create_table(table, columns, dtypes, if_exists="replace"):
+    df = pd.DataFrame(columns=columns)
+    if isinstance(dtypes, list): dtypes = dict(zip(columns, dtypes))
+    df.to_sql(table, con=engine, if_exists=if_exists, dtype=dtypes, index=False)
+
+def delete_table(table):
     tbl = db.Table(table, metadata)
     tbl.drop(engine) #deletes the table
 
-def read_table(table=TABLE_NAME):
+def read_table(table):
     return(pd.read_sql(table, con=engine))
 
-def append_row(data, columns=COLUMN_NAMES, table=TABLE_NAME):
-    if type(data) is list: 
-        data = [data] if type(data[0]) is not list else data
-        data = pd.DataFrame.from_dict(data)
-    elif type(data) is dict:
+def append_row(data, table):
+    # data is DataFrame or dict
+    if type(data) is dict:
         data = pd.DataFrame(data)
     data.to_sql(table, con=engine, if_exists="append", index=False)
 
-def delete_row(row, table=TABLE_NAME):
+def delete_row(row, table):
     # row can be dict or DataFrame with single row
     df = read_table(table)
     tbl = db.Table(table, metadata, autoload=True, autoload_with=engine)
@@ -50,26 +54,53 @@ def delete_row(row, table=TABLE_NAME):
         status = conn.execute(delete)
     return(status)
 
-def add_s3_flows(names="all", table=TABLE_NAME):
+def list_flows(names="all"):
+    columns = ["flow", "run", "start", "finish"]
     if isinstance(names,str) and names=="all": 
         names = []
         flows = Metaflow().flows
         for flow in flows:
             names.append(flow.pathspec)
-    df = pd.DataFrame(columns=COLUMN_NAMES)
+    df = pd.DataFrame(columns= columns)
     for name in names:
         runs = list(Flow(name))
         for run in runs:
             index = run.path_components[1]
             start = run.created_at
             finish = run.finished_at
-            df = df.append(dict(zip(COLUMN_NAMES, [name, index, start, finish])), ignore_index=True)
-    df = df.sort_values(by=COLUMN_NAMES[:2])
+            df = df.append(dict(zip(columns, [name, index, start, finish])), ignore_index=True)
+    return(df.sort_values(by=columns[:2]))
+
+def list_s3_datasets(s3_path="data/", bucket=BUCKET):
+    columns= ["name", "size", "modified"]
+    s3 = boto3.client('s3')
+    objs = s3.list_objects_v2(Bucket=bucket, Prefix=s3_path)
+    df = pd.DataFrame(columns=columns)
+    for obj in objs['Contents']:
+        df = df.append({
+            'name': os.path.basename(obj['Key']),
+            'size': obj['Size'],
+            'modified': obj['LastModified']
+        }, ignore_index=True)
+    df['size'] = pd.to_numeric(df['size'])
+    df['modified'] = pd.to_datetime(df['modified'])
+    return(df)
+
+def reset_models_table(table="models"):
+    df = list_flows()
+    create_table(table, 
+        columns= df.columns.tolist(), 
+        dtypes= db.types.String(32))
     append_row(df, table=table)
 
-def reset_models_table(columns=COLUMN_NAMES, dtype=COLUMN_DTYPES):
-    create_table(TABLE_NAME, columns=columns, dtype=dtype)
-    add_s3_flows(names="all", table=TABLE_NAME)
+def reset_training_table(table="training"):
+    create_table(table, 
+        columns= ["flow", "run", "start"],
+        dtypes= db.types.String(32))
 
-def reset_training_table():
-    return(create_table("training", ["flow", "run", "start"], db.types.String(32)))
+def reset_data_table(table="data"):
+    df = list_s3_datasets()
+    create_table(table, 
+        columns= df.columns.tolist(), 
+        dtypes= [db.types.String(32), db.types.INTEGER(), db.types.DATETIME()])
+    append_row(df, table=table)
